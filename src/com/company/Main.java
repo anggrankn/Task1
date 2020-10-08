88bf5740fb7857bd62104161bef5d47302efd83a
Angelina Grankina <angelina.grankina200214@mail.ru>
2020-10-08 09:59:47 +0300

    task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double side = enterSide();
-        conclusionArea(side);
+        double side = readSide();
+        displayArea(side);
     }
 
-    static double enterSide() {
+    static double readSide() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите значение длины стороны квадрата: ");
         return scanner.nextDouble();
     }
 
-    static void conclusionArea(double side) {
-        System.out.printf("Площадь заштрихованной части фигуры равна: %.3f", pictureArea(side));
+    static void displayArea(double side) {
+        System.out.printf("Площадь заштрихованной части фигуры равна: %.3f", getPictureArea(side));
     }
 
-    static double pictureArea(double side) {
-        return (squireArea(side) - circleArea(side)) / 4 + squireArea(side / 2);
+    static double getPictureArea(double side) {
+        return (getSquireArea(side) - getCircleArea(side)) / 4 + getSquireArea(side / 2);
     }
 
-    static double squireArea(double side) {
+    static double getSquireArea(double side) {
         return side * side;
     }
 
-    static double circleArea(double side) {
+    static double getCircleArea(double side) {
         return Math.PI * side / 2 * side / 2;
     }
 }
